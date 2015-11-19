1c7b30cb09b3c5049b566d41fcd09ddbd1064b90
loszer <loszer@163.com>
2015-11-19 23:53:22 +0800

    regular commit

---
+++ AnalyzeTable.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2015-11-19 23:37:07
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+"""analyze the table of stdudent from the specific page or url"""
+
+#use a list var contains the content of courses in the table
+_table = []
+
+def getTale():
+	return _table
+
+def print2pdf():
+	#print the table to pdf and send it to user's mailbox
+	pass
+
+def analyze():
+	pass
+
+def _analyzetable(spcific_url):
+	#extract the core data of spcific page
+	pass
---
+++ base.py
@@ -0,0 +1,27 @@
+#base.py
+# -*- coding: utf-8 -*-
+
+_user_info = {
+	name:'',
+	username:'',
+	password:'',
+	others:{}
+}
+
+def _login(username ,password):
+	"""the function to login the system in dummy"""
+	pass
+
+def _getname():
+	pass
+
+def _getUserData():
+	return _user_info
+	pass
+
+def analyze(argvs):
+	pass
+
+#send the table to one's mailbox
+def sendtable2one(email_addr):
+	pass
