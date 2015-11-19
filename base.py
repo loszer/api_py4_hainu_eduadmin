00dce3317ad0d51b4e03a23f9fc224ffdfe7c415
loszer <loszer@163.com>
2015-11-19 23:53:22 +0800

    first commit

---
+++ base.py
@@ -0,0 +1,20 @@
+#base.py
+# -*- coding: utf-8 -*-
+
+_user_info = {
+	name:'',
+	username:'',
+	password:'',
+	others:{}
+}
+
+def _login(username ,password):
+	"""the function to login the system in dummy"""
+	pass
+
+def _getname():
+	pass
+
+def getUserData():
+	return _user_info
+	pass
